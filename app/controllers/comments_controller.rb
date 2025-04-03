93e0f4934c8ead11fdf8e0d1355d30fae984e4f6
Liam Ivancic <ivancic.liam@gmail.com>
2025-04-03 14:15:54 +1100

    disable http_auth for benefit of demonstration

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "Liam", password: "123", only: :destroy
+  # http_basic_authenticate_with name: "Liam", password: "123", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
